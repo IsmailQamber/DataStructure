a4fb44eb3615001ec407532823df30b8e3d2d8ac
IsmailQamber <ism-you-95@hotmail.com>
2021-01-18 15:12:07 +0300

    udated

--- tree.js
+++ tree.js
@@ -2,13 +2,14 @@ const prompt = require('prompt-sync')({sigint: true});
 class TreeNode{
     constructor(name){
         this.name = name;
-        this.child = [];
+        this.children = [];
         this.limit = 2; //restricting the child lenght
     }
 
-    addChild = (Node) =>{
-        if(this.child.length < this.limit){ //making sure that the parent only have two children
-            this.child.push(Node);
+    addChild = (node) =>{
+        if(this.children.length < this.limit){ //making sure that the parent only have two children
+            this.child.push(node);
+            console.log(`${this.child},is a child of  ${node.name}`)
         }else{
             console.log("You already have two children, isn't that enough ?")
         }
@@ -21,26 +22,58 @@ class TreeNode{
             nodes = [...nodes, ...current.child];
         }
     }
+    
+    getChildWithName = (name) => {
+        return this.children.find((child) => { 
+            child.name === name 
+        });
+    }
 
 }
-const familyName = prompt("enter the family name: ")
-const root = new TreeNode(familyName);
 
-const child = prompt(`enter child full name:`);
+const root = new TreeNode(Qamber);
 
-while (child !== "done"){
+let fullName = prompt(`enter child full name:`);
+
+while (fullName !== "done"){
     
-    let tree = [];
-    tree = child.split(" ").reverse();
-     if(tree[0] === root.name && root.child.length < 2){
-         child = prompt("enter child name: ");
-         root.addChild(child);
-     }else if(root.child.length === 2){
-         console.log("This parent has enough children")
-     }else{
-         child = prompt("enter child name: ")
-         root.addChild(child);
-     }
+    let current = root;
+
+    let names = fullName.split(" ").reverse();
+    let fistName = names.pop();
+    let lastName = names.shift();
+
+    if (lastName === current.name){
+        if(names){
+            for(let name of names){
+                let child = current.getChildWithName(name);
+                if(child){
+                    current = child;
+                }else {
+                    let newNode = new TreeNode(name);// adds child that did not exist or should should not exist, adding a third child for the root
+                    current.addChild(newNode);
+                    current = newNode;
+                }
+            }
+        }
+        let newNode = new TreeNode(fistName);
+        current.addChild(newNode);
+    }
+
+       console.log("-----------------------")
+       fullName = prompt(`enter child full name:`);     
+        
+    }
+    // tree = child.split(" ").reverse();
+    //  if(tree[0] === root.name && root.child.length < 2){
+    //      child = prompt("enter child name: ");
+    //      root.addChild(child);
+    //  }else if(root.child.length === 2){
+    //      console.log("This parent has enough children")
+    //  }else{
+    //      child = prompt("enter child name: ")
+    //      root.addChild(child);
+    //  }
 
        
     }
